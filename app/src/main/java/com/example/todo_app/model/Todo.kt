550e52d55bd30f605c1e09b3e2e0351f7fae9685
bobby_nodu@hotmail.com <bobby_nodu@hotmail.com>
2023-03-18 19:45:18 +0200

    fixed duplicate class in dependency

--- app/src/main/java/com/example/todo_app/model/Todo.kt
+++ app/src/main/java/com/example/todo_app/model/Todo.kt
@@ -1,5 +1,7 @@
 package com.example.todo_app.model
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import java.util.concurrent.CountedCompleter
 
@@ -9,3 +11,24 @@ data class Todo(
     var title: String,
     var completed: Boolean
 )
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/todos"
+
+interface TodosApi {
+    @GET("todos")
+    suspend fun getTodos(): List<Todo>
+
+    companion object {
+        private var todosService: TodosApi? = null
+
+        fun getInstance(): TodosApi {
+            if (todosService === null) {
+                todosService = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build().create(TodosApi::class.java)
+            }
+            return todosService!!
+        }
+    }
+}
--- app/src/main/java/com/example/todo_app/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/todo_app/viewmodel/TodoViewModel.kt
@@ -1,16 +1,16 @@
 package com.example.todo_app.viewmodel
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todo_app.model.Todo
+import com.example.todo_app.model.TodosApi
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
+
 
 class TodoViewModel: ViewModel() {
-    var todos = mutableListOf<Todo>()
+    var todos = mutableStateListOf<Todo>()
         private set
 
     init {
@@ -31,23 +31,3 @@ class TodoViewModel: ViewModel() {
     }
 }
 
-const val BASE_URL = "https://jsonplaceholder.typicode.com/todos"
-
-interface TodosApi {
-    @GET("todos")
-    suspend fun getTodos(): List<Todo>
-
-    companion object {
-        var todosService: TodosApi? = null
-
-        fun getInstance(): TodosApi {
-            if (todosService === null) {
-                todosService = Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build().create(TodosApi::class.java)
-            }
-            return todosService!!
-        }
-    }
-}
